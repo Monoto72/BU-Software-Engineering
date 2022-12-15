2107faa78e04d48ab438ecfec5c21f506ae0e989
Artlenhavis123 <oliver.havis@gmail.com>
2022-12-15 23:29:30 +0000

    Added API endpints

---
+++ src/main/api_functions.py
@@ -0,0 +1,82 @@
+# API Functions
+import json
+import os
+import random
+
+# Get path to the recipes.json file
+path = os.path.join(os.path.dirname(__file__), 'recipes.json')
+
+# Get recipes from an array of ingredients
+def getRecipes(ingredients):
+    print(ingredients)
+    # Open the recipe.json file
+    with open(path, 'r') as f:
+        # Loads the json file
+        recipes = json.load(f)
+        
+        # Extract the list of recipes from the data
+        recipes = recipes['recipes']
+        
+        # Set up an empty list to store matching recipes
+        matching_recipes = []
+        
+        # Loop through each recipe
+        for recipe in recipes:
+            # Extract the list of ingredients from the recipe
+            recipe_ingredients = recipe['ingredients']
+
+            # loop though each ingredient in the array
+            for ingredient in ingredients:
+                
+                # Loops through each ingredient in the recipe
+                for recipe_ingredient in recipe_ingredients:
+                    if ingredient in recipe_ingredient:
+                        matching_recipes.append(recipe)
+                        break
+
+    # Return the list of matching recipes as a JSON response
+    return {'recipes': matching_recipes}
+
+# Get recipes by name
+def getRecipeByName(recipe_filter = 'all', page=0, per_page=5):
+    # open the recipe.json file
+    with open(path, 'r') as f:
+        # Loads the json file
+        recipes = json.load(f)
+        
+        # Extract the list of recipes from the data
+        recipes = recipes['recipes']
+        
+        # Set up an empty list to store matching recipes
+        matching_recipes = []
+        
+        # Checks the recipe filter 
+        if recipe_filter == 'all':
+            
+            for i in range(page, page + per_page):
+                if i >= len(recipes):
+                    break
+                matching_recipes.append(recipes[i])
+        
+        else:
+            for i in range(page, page + per_page):
+                if i >= len(recipes):
+                    break
+                if recipe_filter in recipes[i]['recipe']:
+                    matching_recipes.append(recipes[i])
+                
+        return matching_recipes
+
+# Random Recipe
+def getRandomRecipe():
+    # Open the recipe.json file
+    with open(path, 'r') as f:
+        # Loads the json file
+        recipes = json.load(f)
+        
+        # Pick a random number
+        rand = random.randint(0, len(recipes['recipes']) - 1)
+        
+        randomRecipe = recipes['recipes'][rand]
+        
+        return randomRecipe
--- src/main/views.py
+++ src/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from . import api_functions as api
 
 # Create your views here.
 
@@ -9,6 +10,19 @@ def index(request):
 
 def recipes(request):
     url = 'recipes'
+
+    if request.method == 'POST':
+        # Get the recipe name, starting positsion and offset from the URL
+        recipe = request.POST['recipe']
+        page = request.POST['page']
+        per_page = request.POST['per_page']
+    
+        recipes = api.getRecipeByName(recipe_filter=recipe, page=page, per_page=per_page) # Get the recipes from the API
+    if request.method == 'GET':
+        recipes = api.getRecipeByName(page=0, per_page=5) # Get the recipes from the API
+        
+    # Need to display it on the page
+        
     return render(request, 'recipes.html', {'page_url': url})
 
 def search(request):
@@ -23,11 +37,17 @@ def search(request):
             if key == field:
                 ingredients.append(request.POST[key])
                 count += 1
-                
-        # getRecipe(ingredients)
+
+        # TODO: Place the result on the Page
+        recipes = api.getRecipes(ingredients)
+        recipes = recipes['recipes'] # Extract the list of recipes from the data
     
     return render(request, 'search.html', {'page_url': url})
 
 def random(request):
     url = 'random'
+    
+    # TODO: Place the result on the Page
+    randomRecipe = api.getRandomRecipe()
+    
     return render(request, 'random.html', {'page_url': url})
